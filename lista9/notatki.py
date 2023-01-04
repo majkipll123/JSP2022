d6ec4aba44b7ccb13859f2382738939c28d48200
Karol Makarowski <majkipll123@hotmail.com>
2023-01-04 13:16:57 +0100

    andrzej

---
+++ lista9/notatki.py
@@ -0,0 +1,25 @@
+z1
+metoda macierzowa 
+uklad rownan zapisujemy jako maciez 
+zobaczyc na neciecos ze a do -1 b
+sami napisac algorytm albo r=zrobic nie myslac o tym 
+
+import numpy as np
+np.linalg.solve(a,b) A(x)=b
+np.allclose
+poprawnosc rozwiazania wymnozyc maciez a z x i wyrzuci true albo false 
+z2
+(1)
+>python main.py 1 2 3 4  zczytac z biblopteki import sys 
+sys.argv
+np.average
+np.var
+np.std
+string na liczby uwaga na bledy przy dodawaniu ciagu slow i liter
+(2)
+wykryc czy w lini komend sa liczby czy text zabrac dane i cos tam porobic 
+z3
+Vo i alfa input()
+rysowanie na wykresie biblioteka mat plot lib .py plot 
+z4
+bar w mat plot lib podawanie na osi y title xlabel ylabel
